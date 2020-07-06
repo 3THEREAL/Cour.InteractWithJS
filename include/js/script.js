99f293bf7459989ca3876a10fc837240f036cc3d
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2020-07-06 22:44:26 +0600

    new file:   JS-Gallery.html 	new file:   include/css/gallery.css 	modified:   include/css/style.css 	new file:   include/js/gallery.js 	modified:   include/js/script.js 	modified:   mod-dom2.html 	new file:   this.html

--- include/js/script.js
+++ include/js/script.js
@@ -23,7 +23,7 @@ function openMe(){
       x.style.display="block";
   }
   */
-
+/*
   function closeMe(){
       x=document.getElementById("demo");
       x.style.display="none";
@@ -32,4 +32,13 @@ function openMe(){
   function openMe(){
       x=document.getElementById("demo");
       x.style.display="block";
+  }
+
+  function message(msg){
+      document.getElementById('output').innerHTML = msg + "event";
+  }
+  */
+
+  function displayId(elemnt){
+      console.log(elemnt.id);
   }
